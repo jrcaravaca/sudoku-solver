97cd362b09cd5ca69f2b7ce0f75b3b698d4c7338
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2025-11-13 06:35:00 +0100

    Add: - Función backtracking, de momento tira error al probar. Pendiente arreglar, pero el algoritmo debería ser correcto

--- src/js/utils/backtraking.js
+++ src/js/utils/backtraking.js
@@ -1,11 +1,24 @@
-import { makeArrayFromBoard } from "./makeArrayFromBoard";
-import { makeNumberArrayFromBoard } from "./makeNumberArrayFromBoard";
+import { isValid } from "./isValid";
 
-export function backtracking() {
-    const cellsArray = makeArrayFromBoard(); 
-    const numberArray = makeNumberArrayFromBoard();
-    let isFinished = false;  
-    console.log(numberArray); 
+export function backtracking(numberArray) {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
 
-    
+            if (numberArray[i][j] === 0) {
+                for (let h = 1; h <= 9; h++) {
+                    if (isValid(numberArray, i, j, h)) {
+                        numberArray[i][j] = h; 
+                        if (backtracking() === true) {
+                            return true; 
+                        } else {
+                            numberArray[i][j] = 0; 
+                        }
+                    } 
+                }
+                return false
+            }
+            
+        }
+    }
+    return true
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './style.css'
 import { board } from './js/utils/board'; 
 import { backtracking } from './js/utils/backtraking';
+import {makeArrayFromBoard} from './js/utils/makeArrayFromBoard'; 
+import {makeNumberArrayFromBoard} from './js/utils/makeNumberArrayFromBoard'
+
 
 const sudokuGrid = document.getElementById('sudoku-grid'); 
 const solveBtn = document.getElementById('solve-btn')
@@ -8,5 +11,9 @@ board(sudokuGrid);
 
 solveBtn.addEventListener('click', (e) => {
     e.preventDefault
-    backtracking()
+    const numberArray = makeNumberArrayFromBoard()
+    const cellsArray = makeArrayFromBoard()
+    backtracking(numberArray)
+    
+    console.log(numberArray)
 })
